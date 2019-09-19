c8f0ae769cc78855971280d054d26002a544e18b
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-19 18:16:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,7 +27,8 @@ class Student
   
   def save
     sql <<-SQL
-      "INSERT INTO students name, grade FROM students 
+      "INSERT INTO students (name, grade) VALUES (?,?,?) name, grade;" 
     SQL
+    @id = 
   end
 end
