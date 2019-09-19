aef8f6aa23c81ffb9b2483c796a08f26677f5606
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-19 18:25:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,9 @@ class Student
   end
   
   def self.drop_table
-    sql = self.create_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
+      SQL
     DB[:conn].execute(sql)
   end
   
